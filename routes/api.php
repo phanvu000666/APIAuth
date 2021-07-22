a6ba5cf9b6c38907020a88ca58689c0fb18b1992
vu lon <vununglon@concac.com>
2021-07-22 16:44:28 +0700

    first commit

--- routes/api.php
+++ routes/api.php
@@ -17,19 +17,33 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-//Api Product
-//Lấy bảng Product
-Route::get('product', 'Product\ProductController@product');
-//Lấy Product dựa vào ID
-Route::get('product/{id}', 'Product\ProductController@productByID');
-//Thêm Product
-Route::post('product', 'Product\ProductController@productSave');
-//Sửa Product
-Route::put('product/{product}', 'Product\ProductController@productUpdate');
-//Xoá Product
-Route::delete('product/{product}', 'Product\ProductController@productDelete');
-//Api Register
-Route::post('register','APIUser\UserApiController@register');
-//Api Login
-Route::post('login','APIUser\UserApiController@login');
+
+// //Api Register
+// Route::post('register','APIUser\UserApiController@register');
+// //Api Login
+// Route::post('login','APIUser\UserApiController@login');
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'APIUser\UserApiController@login');
+    Route::post('signup', 'APIUser\UserApiController@signup');
+  
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'APIUser\UserApiController@logout');
+        Route::get('user', 'APIUser\UserApiController@user');
+        //Api Product
+    //Lấy bảng Product
+    Route::get('product', 'Product\ProductController@product');
+    //Lấy Product dựa vào ID
+    Route::get('product/{id}', 'Product\ProductController@productByID');
+    //Thêm Product
+    Route::post('product', 'Product\ProductController@productSave');
+    //Sửa Product
+    Route::put('product/{product}', 'Product\ProductController@productUpdate');
+    //Xoá Product
+    Route::delete('product/{product}', 'Product\ProductController@productDelete');
+    });
+});
 
